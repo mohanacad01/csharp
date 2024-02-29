dde8183997465f61ef6e1591ebd59e96ea7294d8
mohanacad01 <mohanacad01@brightskills.com>
2024-02-29 17:52:05 +0530

    added files

--- myproject/Controllers/HomeController.cs
+++ myproject/Controllers/HomeController.cs
@@ -28,6 +28,16 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
